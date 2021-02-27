3a3bb53aa293c6f83d860c703f33990dfc7254fe
Arijit Deb <arijitdeb.git@gmail.com>
2021-02-28 06:07:03 +0800

    Adding to trigger jenkins job

--- src/test/java/stepdefinitions/InsurancePageFeatureSteps.java
+++ src/test/java/stepdefinitions/InsurancePageFeatureSteps.java
@@ -17,7 +17,7 @@ import cucumber.api.java.en.When;
 import pageobjectrepo.LoginPage;
 import pageobjectrepo.InsuranceHomePage;
 
-//Git webhook trigger jenkins agagin
+//Git webhook trigger jenkins agagin again
 
 public class InsurancePageFeatureSteps extends Common{
 	InsuranceHomePage ihp;
