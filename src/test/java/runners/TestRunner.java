98dbaf682f8fa41c441fca8aff86a853749f5e81
Arijit Deb <arijitdeb.git@gmail.com>
2021-02-28 17:29:11 +0800

    Added Calendar testing scenario and step definitions

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -3,6 +3,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
  
+//trigger from jenkins
 @RunWith(Cucumber.class)
 @CucumberOptions(
  features = "src/test/resources/functionalTests",
